aa62e248063152ae4a8d86345feb6fcda3aceb8f
Ross Schlaikjer <ross@schlaikjer.net>
2018-03-21 22:37:10 -0400

    Allocate large matrices on the heap
    Prevents long string comparisons from trampling the stack

--- c_src/levenshtein.c
+++ c_src/levenshtein.c
@@ -7,6 +7,16 @@
 int levenshtein(unsigned char *s1, unsigned s1len,
                 unsigned char *s2, unsigned s2len);
 
+int levenshtein_stack(unsigned char *s1, unsigned s1len,
+                      unsigned char *s2, unsigned s2len);
+
+int levenshtein_heap(unsigned char *s1, unsigned s1len,
+                     unsigned char *s2, unsigned s2len);
+
+int levenshtein_impl(unsigned char *s1, unsigned s1len,
+                     unsigned char *s2, unsigned s2len,
+                     unsigned int *matrix);
+
 ERL_NIF_TERM mk_atom(ErlNifEnv* env, const char* atom) {
     ERL_NIF_TERM ret;
     if(!enif_make_existing_atom(env, atom, &ret, ERL_NIF_LATIN1)) {
@@ -15,26 +25,26 @@ ERL_NIF_TERM mk_atom(ErlNifEnv* env, const char* atom) {
     return ret;
 }
 
-ERL_NIF_TERM mk_error(ErlNifEnv* env, ERL_NIF_TERM msg) {
-    return enif_make_tuple2(env, mk_atom(env, "error"), msg);
+ERL_NIF_TERM mk_error(ErlNifEnv* env, const char* mesg) {
+    return enif_make_tuple2(env, mk_atom(env, "error"), mk_atom(env, mesg));
 }
 
 static ERL_NIF_TERM erl_levenshtein(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+    ErlNifBinary binary1;
+    ErlNifBinary binary2;
+
     // Assert we got two arguments
     if(argc != 2) {
         return enif_make_badarg(env);
     }
 
     // Ensure that the args are binaries
-    if(!enif_is_binary(env, argv[0])) {
-        return mk_error(env, enif_make_tuple2(env, mk_atom(env, "not_a_binary"), argv[0]));
-    }
-    if(!enif_is_binary(env, argv[1])) {
-        return mk_error(env, enif_make_tuple2(env, mk_atom(env, "not_a_binary"), argv[1]));
+    if(!enif_is_binary(env, argv[0])
+        || !enif_is_binary(env, argv[1])) {
+        return mk_error(env, "not_a_binary");
     }
 
-    // Extract the binary metadata
-    ErlNifBinary binary1, binary2;
+    // Copy the binary metadata into the structs
     enif_inspect_binary(env, argv[0], &binary1);
     enif_inspect_binary(env, argv[1], &binary2);
 
@@ -44,9 +54,12 @@ static ERL_NIF_TERM erl_levenshtein(ErlNifEnv* env, int argc, const ERL_NIF_TERM
         binary2.data, binary2.size
     );
 
-    // Wrap it as a term & return
     return enif_make_int(env, editDistance);
 }
+
+static ErlNifFunc nif_funcs[] = {
+    {"levenshtein", 2, erl_levenshtein}
+};
 
 // Module callbacks
 int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info) {
@@ -60,32 +73,81 @@ int upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_NIF_TERM
 void unload(ErlNifEnv* env, void* priv_data){
 }
 
-// Method definitions
-static ErlNifFunc nif_funcs[] = {
-    {"levenshtein", 2, erl_levenshtein}
-};
 
-// Register the NIF
 ERL_NIF_INIT(levenshtein, nif_funcs, load, NULL, upgrade, unload);
-
-// Actual C levenshtein implementation
 
 int levenshtein(unsigned char *s1, unsigned s1len,
                 unsigned char *s2, unsigned s2len) {
-    unsigned int x, y;
-    unsigned int matrix[s2len+1][s1len+1];
-    matrix[0][0] = 0;
-    for (x = 1; x <= s2len; x++)
-        matrix[x][0] = matrix[x-1][0] + 1;
-    for (y = 1; y <= s1len; y++)
-        matrix[0][y] = matrix[0][y-1] + 1;
-    for (x = 1; x <= s2len; x++)
-        for (y = 1; y <= s1len; y++)
-            matrix[x][y] = MIN3(
-                matrix[x-1][y] + 1,
-                matrix[x][y-1] + 1,
-                matrix[x-1][y-1] + (s1[y-1] == s2[x-1] ? 0 : 1)
-            );
+    // If the matrix is over 1Kib, allocate it on the heap
+    if (s1len * s2len > 1024) {
+        return levenshtein_heap(s1, s1len, s2, s2len);
+    } else {
+        return levenshtein_stack(s1, s1len, s2, s2len);
+    }
+}
 
-    return(matrix[s2len][s1len]);
+int levenshtein_stack(unsigned char *s1, unsigned s1len,
+                      unsigned char *s2, unsigned s2len) {
+    unsigned int matrix[s2len+1][s1len+1];
+    return levenshtein_impl(s1, s1len, s2, s2len, (unsigned int *) matrix);
+}
+
+int levenshtein_heap(unsigned char *s1, unsigned s1len,
+                      unsigned char *s2, unsigned s2len) {
+    unsigned int *matrix = malloc(
+        sizeof(unsigned int) * (s1len + 1) * (s2len + 1)
+    );
+    int result = levenshtein_impl(s1, s1len, s2, s2len, matrix);
+    free(matrix);
+    return result;
+}
+
+void set_matrix_element(unsigned int *matrix, unsigned int xsize,
+                     unsigned int x, unsigned int y,
+                     unsigned int value) {
+    matrix[x * xsize + y] = value;
+}
+
+unsigned int get_matrix_element(unsigned int *matrix, unsigned int xsize,
+                                unsigned int x, unsigned int y) {
+    return matrix[x * xsize + y];
+}
+
+int levenshtein_impl(unsigned char *s1, unsigned s1len,
+                     unsigned char *s2, unsigned s2len,
+                     unsigned int *matrix) {
+    const unsigned int xsize = s1len + 1;
+    unsigned int x, y;
+    matrix[0] = 0;
+    for (x = 1; x <= s2len; x++) {
+        set_matrix_element(
+            matrix, xsize, x, 0,
+            get_matrix_element(matrix, xsize, x-1, 0) + 1
+        );
+    }
+    for (y = 1; y <= s1len; y++) {
+        set_matrix_element(
+            matrix, xsize, 0, y,
+            get_matrix_element(matrix, xsize, 0, y - 1) + 1
+        );
+    }
+    for (x = 1; x <= s2len; x++) {
+        for (y = 1; y <= s1len; y++) {
+            //matrix[x][y] = MIN3(
+            set_matrix_element(
+                matrix, xsize,
+                x, y,
+                MIN3(
+                    // matrix[x-1][y] + 1,
+                    get_matrix_element(matrix, xsize, x-1, y) + 1,
+                    // matrix[x][y-1] + 1,
+                    get_matrix_element(matrix, xsize, x, y-1) + 1,
+                    // matrix[x-1][y-1] + (s1[y-1] == s2[x-1] ? 0 : 1)
+                    get_matrix_element(matrix, xsize, x-1, y-1) + (s1[y-1] == s2[x-1] ? 0 : 1)
+                )
+            );
+        }
+    }
+
+    return get_matrix_element(matrix, xsize, s2len, s1len);
 }
